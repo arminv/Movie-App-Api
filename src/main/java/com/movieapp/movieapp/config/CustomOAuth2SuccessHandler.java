440d056f2aacbb825d750ab1b2ce39d4985c948e
Armin Varshokar <arminvarshokar@gmail.com>
2024-09-30 02:22:40 -0400

    Formatting

--- src/main/java/com/movieapp/movieapp/config/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/movieapp/movieapp/config/CustomOAuth2SuccessHandler.java
@@ -1,15 +1,14 @@
 package com.movieapp.movieapp.config;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-
-
 import java.io.IOException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import com.movieapp.movieapp.services.UserService;
 
