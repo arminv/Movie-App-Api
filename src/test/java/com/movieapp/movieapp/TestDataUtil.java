66faff24f497d7769f3e68810cf80019c62ebf04
Armin Varshokar <arminvarshokar@gmail.com>
2024-10-13 06:45:21 -0400

    Add more integration tests for movie controller

--- src/test/java/com/movieapp/movieapp/TestDataUtil.java
+++ src/test/java/com/movieapp/movieapp/TestDataUtil.java
@@ -6,12 +6,12 @@ import com.movieapp.movieapp.movie.Movie;
 import com.movieapp.movieapp.movie.MovieDto;
 import com.movieapp.movieapp.user.User;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public final class TestDataUtil {
 
     final static String TEST_USER_ID = "54hh849n9on7bu56uberf";
-
-    private TestDataUtil() {
-    }
 
     public static User createTestUserEntityA() {
         final LocalDateTime dateCreated = LocalDateTime.parse("2024-10-11T18:34:14.406");
@@ -45,15 +45,15 @@ public final class TestDataUtil {
         final LocalDateTime dateCreated = LocalDateTime.parse("2017-03-01T10:10:10.100");
 
         return MovieDto.builder()
-            .id("39gjw9f93jgbnwcvdv023")
-            .name("The Green Mile")
+            .id("39vkjwlc02jfg9ejhv9evce")
+            .name("Harry Potter")
             .userId(TEST_USER_ID)
-            .movieDBId("20vkbw94ngswom")
+            .movieDBId("29jh23iv92cjo")
             .created(dateCreated)
             .lastUpdated(dateCreated)
             .dateWatched(dateCreated)
-            .rating(5.0f)
-            .review("One of the best movies ever!")
+            .rating(3.5f)
+            .review("Not a bad movie at all!")
             .build();
     }
 
@@ -70,6 +70,22 @@ public final class TestDataUtil {
             .dateWatched(dateCreated)
             .rating(4.5f)
             .review("One of the best movies!")
+            .build();
+    }
+
+    public static MovieDto createTestMovieDtoC() {
+        final LocalDateTime dateCreated = LocalDateTime.parse("2014-01-05T11:11:11.200");
+
+        return MovieDto.builder()
+            .id("0kf0v282jcscjn2008c,")
+            .name("Lost")
+            .userId(TEST_USER_ID)
+            .movieDBId("298vnqw83b55t8v")
+            .created(dateCreated)
+            .lastUpdated(dateCreated)
+            .dateWatched(dateCreated)
+            .rating(4.0f)
+            .review("Great series to watch.")
             .build();
     }
 
--- src/test/java/com/movieapp/movieapp/movie/MovieControllerIntegrationTests.java
+++ src/test/java/com/movieapp/movieapp/movie/MovieControllerIntegrationTests.java
@@ -1,13 +1,11 @@
 package com.movieapp.movieapp.movie;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -41,7 +39,7 @@ public class MovieControllerIntegrationTests extends BaseMongoTest {
         String movieJson = objectMapper.writeValueAsString(testMovieA);
 
         mockMvc.perform(
-            MockMvcRequestBuilders.put("/movies/39gjw9f93jgbnwcvdv023")
+            MockMvcRequestBuilders.put("/movies/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(movieJson)
         ).andExpect(
@@ -50,7 +48,7 @@ public class MovieControllerIntegrationTests extends BaseMongoTest {
     }
 
     @Test
-    public void testThatUpdateMovieReturnsHttpStatus200Ok() throws Exception {
+    public void testThatUpdateMovieReturnsHttpStatus201Ok() throws Exception {
         Movie testMovieEntityA = TestDataUtil.createTestMovieEntityA();
         Movie savedMovieEntity = movieService.createUpdateMovie(
             testMovieEntityA.getId(), CreateUpdateMovieRequest.builder()
@@ -73,6 +71,50 @@ public class MovieControllerIntegrationTests extends BaseMongoTest {
                 .content(movieJson)
         ).andExpect(
             MockMvcResultMatchers.status().isCreated()
+        );
+    }
+
+    @Test
+    public void testThatCreateMovieReturnsCreatedMovie() throws Exception {
+        MovieDto testMovieC = TestDataUtil.createTestMovieDtoC();
+        String movieJson = objectMapper.writeValueAsString(testMovieC);
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.put("/movies/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(movieJson)
+        ).andExpect(
+            MockMvcResultMatchers.jsonPath("$.name").value("Lost")
+        ).andExpect(
+            MockMvcResultMatchers.jsonPath("$.rating").value(4.0f)
+        );
+    }
+
+    @Test
+    public void testThatUpdateMovieReturnsUpdatedMovie() throws Exception {
+        Movie testMovieEntityA = TestDataUtil.createTestMovieEntityA();
+        Movie savedMovieEntity = movieService.createUpdateMovie(
+            testMovieEntityA.getId(), CreateUpdateMovieRequest.builder()
+                .movieDBId(testMovieEntityA.getId())
+                .name(testMovieEntityA.getName())
+                .userId(testMovieEntityA.getUserId())
+                .rating(testMovieEntityA.getRating())
+                .review(testMovieEntityA.getReview())
+                .dateWatched(testMovieEntityA.getDateWatched())
+                .build()
+        );
+
+        MovieDto testMovieC = TestDataUtil.createTestMovieDtoC();
+        testMovieC.setId(savedMovieEntity.getId());
+        testMovieC.setMovieDBId(savedMovieEntity.getMovieDBId());
+        String movieJson = objectMapper.writeValueAsString(testMovieC);
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.put("/movies/" + testMovieC.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(movieJson)
+        ).andExpect(
+            MockMvcResultMatchers.jsonPath("$.rating").value(4.0f)
         );
     }
 
